bade5af8316728b6b84c6130f6f4461782dfd08a
Essietasha <163308907+Essietasha@users.noreply.github.com>
2024-05-19 14:09:02 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 const timer = document.querySelector('.timer');
+let days;
+let weeks;
+let months;
+let years;
+let userAge;
+
+const userAgeInput = document.getElementById('ageInput');
+userAge = userAgeInput.value;
+
+
+
 function loadTime ()  {
  setInterval(() => {
     let thisDate = new Date();
@@ -25,20 +36,21 @@ const message = document.querySelector('.result');
 function runAgeCounter (e) {
     e.preventDefault();
     const userNameInput = document.getElementById('nameInput');
-    const userAgeInput = document.getElementById('ageInput');
     const nameValue = userNameInput.value;
-    const ageValue = userNameInput.value;
+    // const userAgeInput = document.getElementById('ageInput');
+    // userAge = userAgeInput.value;
 
-    message.innerHTML = `Hello, ${nameValue}, you have -- days,  -- months and -- weeks left to walk through life.`;
+    message.innerHTML = `Hello ${nameValue}, we assume you have a 100 years to live. You have ${days} DAYS,  ${weeks} WEEKS and  ${months} MONTHS to live. Make it count!`;
 };
 formSubmission.addEventListener('submit', runAgeCounter);
 
 function ageCounter(age) {
-    let remainingYears = 90 - age;
-    let days = remainingYears * 365;
-    let weeks= remainingYears * 52;
-    let months = remainingYears * 12;
-    console.log(months)
+
+    let remainingYears = 100 - age;
+     days = remainingYears * 365;
+     weeks= remainingYears * 52;
+     months = remainingYears * 12;
+    //  years = 100 - userAge;
 }
     
-ageCounter(56);
+ageCounter(30);
